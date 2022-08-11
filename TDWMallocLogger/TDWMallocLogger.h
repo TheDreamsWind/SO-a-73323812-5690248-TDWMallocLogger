bcdbe790a96d7e688cfbc3f43b7a997067383063
Aleksandr Medvedev <the.dreams.wind@gmail.com>
2022-08-11 14:38:05 +0300

    Initial Commit

---
+++ TDWMallocLogger/TDWMallocLogger.h
@@ -0,0 +1,12 @@
+//
+//  TDWMallocLogger.h
+//  TDWMallocLogger
+//
+//  Created by Aleksandr Medvedev on 11.08.2022.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface TDWMallocLogger : NSObject
+
+@end
---
+++ TDWMallocLogger/TDWMallocLogger.m
@@ -0,0 +1,12 @@
+//
+//  TDWMallocLogger.m
+//  TDWMallocLogger
+//
+//  Created by Aleksandr Medvedev on 11.08.2022.
+//
+
+#import "TDWMallocLogger.h"
+
+@implementation TDWMallocLogger
+
+@end
